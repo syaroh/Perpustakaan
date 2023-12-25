cbd701f0a4589324faa3a19bdb8916216dffde54
syaroh <rohimatulmaisyaroh@gmail.com>
2023-12-25 07:25:23 +0700

    menambah bootstrap

--- perpus/settings.py
+++ perpus/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for perpus project.
 
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'perpus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'perpus',
+        'USER' : 'root',
+        'PASSWORD' : 'syaroh123',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
@@ -116,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
